8f2c4b60073685f53cd83ea04e7bc060a1b9a4a9
Erdinc Atilgan <erdinc.atilgan@gmail.com>
2015-07-14 22:28:18 -0400

    agin

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,8 +29,8 @@ user_index.each do |n|
                     email: email ,
                     password:              'xxxxxx' ,
                     password_confirmation: 'xxxxxx' ,
-                    # image_url: Faker::Avatar.image)
-                    image_url: "face"+rand(1..10).to_s+".JPG")
+                    image_url: Faker::Avatar.image)
+                    # image_url: "face"+rand(1..10).to_s+".JPG")
   (rand(10)+0).times do
     user.products.create( name: Faker::Commerce.product_name ,
                           price: Faker::Commerce.price ,
